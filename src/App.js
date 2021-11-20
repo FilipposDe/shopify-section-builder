c75393f926f413e3a46b5a5469a16c7b024f99ef
Filippos <legolas19902001@yahoo.gr>
2021-11-21 01:33:35 +0200

    polaris

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     result += "\n";
 
     // HTML
-    result += `<section class='section-${name}'>\n`;
+    result += `<section className='section-${name}'>\n`;
     if (settings.some((s) => s.id === "content")) {
       result += `  {{ section.settings.content }}\n`;
     }
@@ -76,32 +76,77 @@ function App() {
   };
   return (
     <>
-      <div>
-        <form>
-          <h3>Info</h3>
-          <p>Name</p>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <p>Add setting:</p>
-          <h3>Layout</h3>
-          <p>Margin</p>
-          <input
-            type="checkbox"
-            onClick={(e) => toggleMargin(e.target.checked)}
-          />
-          <p>Content</p>
-          <input
-            type="checkbox"
-            onClick={(e) => toggleContent(e.target.checked)}
-          />
-          <h3>Blocks</h3>
-          {/* <button onClick={addTextBlock}>Text</button> */}
-          {/* <button onClick={addImageBlock}>Image</button> */}
-        </form>
+      <div class="Polaris-Page">
+        <div class="Polaris-Page__Content">
+          <div class="Polaris-Layout">
+            <div class="Polaris-Layout__AnnotatedSection">
+              <div class="Polaris-Layout__AnnotationWrapper">
+                <div class="Polaris-Layout__Annotation">
+                  <div class="Polaris-TextContainer">
+                    <h2 class="Polaris-Heading">Form</h2>
+                    <p>A sample form using Polaris components.</p>
+                  </div>
+                </div>
+                <div class="Polaris-Layout__AnnotationContent">
+                  <div class="Polaris-Card">
+                    <div class="Polaris-Card__Section">
+                      <div class="Polaris-FormLayout">
+                        <div role="group" class="">
+                          <div class="Polaris-FormLayout__Items">
+                            <div class="Polaris-FormLayout__Item">
+                              <div class="">
+                                <div class="Polaris-Labelled__LabelWrapper">
+                                  <div class="Polaris-Label">
+                                    <label
+                                      id="TextField1Label"
+                                      for="TextField1"
+                                      class="Polaris-Label__Text"
+                                    >
+                                      Section name
+                                    </label>
+                                  </div>
+                                </div>
+                                <div class="Polaris-TextField">
+                                  <input
+                                    id="TextField1"
+                                    placeholder="Featured"
+                                    class="Polaris-TextField__Input"
+                                    aria-labelledby="TextField1Label"
+                                    aria-invalid="false"
+                                    type="text"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                  />
+                                  <div class="Polaris-TextField__Backdrop"></div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <br />
+      <br />
+      <br />
+
+      <p>Add setting:</p>
+      <h3>Layout</h3>
+      <p>Margin</p>
+      <input type="checkbox" onClick={(e) => toggleMargin(e.target.checked)} />
+      <p>Content</p>
+      <input type="checkbox" onClick={(e) => toggleContent(e.target.checked)} />
+      <h3>Blocks</h3>
+      {/* <button onClick={addTextBlock}>Text</button> */}
+      {/* <button onClick={addImageBlock}>Image</button> */}
+      <br />
       <pre>
         {result}
         {"{% schema %}"}
